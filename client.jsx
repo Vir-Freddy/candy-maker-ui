91d1fcda9b407c15f0127c5c72d8edf936d7a1e3
Freddy Kangai <fkangai@caremetx.com>
2021-01-05 18:19:26 -0500

    semi working config save

--- client.jsx
+++ client.jsx
@@ -1,1 +1,9 @@
 
+import React from 'react'
+import { render } from 'react-dom'
+import Manufacturers from './components/Manufacturers'
+
+render(
+  <Manufacturers />,
+  document.getElementById('root'),
+)
