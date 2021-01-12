b756425f536dee1fa44fec3c35a3cf2b220dfa20
Freddy Kangai <fkangai@caremetx.com>
2021-01-11 23:27:53 -0500

    working elements rendering from the searchable list

--- client.jsx
+++ client.jsx
@@ -1,9 +1,20 @@
 
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Manufacturers from './components/Manufacturers'
 
+import ErrorPage from './pages/Error'
+import ManufacturersPage from './pages/Manufacturers'
+import ProductsPage from './pages/Products'
+
 render(
-  <Manufacturers />,
+  <BrowserRouter>
+    <Switch>
+     <Route path="/products" component={ProductsPage} />
+      <Route exact path="/" component={Manufacturers} />
+      <Route path="*" component={ErrorPage} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root'),
 )
